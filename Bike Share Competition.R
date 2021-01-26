d89762bf010d21da0c4379ad6eff669678aea6c0
Gregory Paulukaitis <gspskillet@gmail.com>
2021-01-25 21:58:07 -0700

    Second commit

---
+++ Bike Share Competition.R
@@ -0,0 +1,82 @@
+#Code for analyzing Bike Sharing System
+
+#Libraries I Need
+library(tidyverse)
+library(DataExplorer)
+library(caret)
+library(vroom)
+library(lubridate)
+library(car)
+
+#read in data
+bike.train <- read.csv("C:\\Users\\gspsk\\Downloads\\train.csv")
+bike.test <- read.csv("C:\\Users\\gspsk\\Downloads\\test.csv")
+bike <- bind_rows(train = bike.train, test = bike.test, .id = "id")
+
+##Drop casual and registered
+bike <- bike %>% select(-casual, -registered)
+
+##Feature Engineering (what features do I really want to use)
+bike$month <- month(bike$datetime) %>% as.factor()
+bike$season <- as.factor(bike$season)
+bike$holiday <- as.factor(bike$holiday)
+
+bike$hour <- hour(bike$datetime) %>% as.factor()
+
+#Exploratory Plots
+qplot(1:nrow(bike.train), bike.train$count, geom = "point")
+
+ggplot(data = bike.train, aes(x = datetime, y = count, color = as.factor(month(datetime)))) +
+  geom_point()
+
+plot_missing(bike)
+
+plot_correlation(bike, type = "continuous", 
+                 cor_args = list(use = 'pairwise.complete.obs'))
+
+ggplot(data = bike, aes(x = season, y = count)) +
+  geom_boxplot()
+
+##look for correlations to determine what vars are unnecessary, like temp and atemp, 
+##more variables, more unstable calculations
+
+##Dummy variable encoding - one-hot encoding
+dummyVars(count ~ season, data = bike, sep = "_") %>% 
+  predict(bike) %>% as.data.frame() %>% 
+  bind_cols(bike %>% select(-season), .)
+
+## Target encoding
+bike$season <- lm(count ~ season, data = bike) %>% 
+  predict(., newdata = bike %>% select(-count))
+
+## Fit some models
+#?train
+
+bike.model <- train(form = count ~ season + holiday + atemp + weather + hour,
+                    data = bike %>% filter(id == 'train'),
+                    method = "ranger",
+                    tuneLength = 8, 
+                    trControl = trainControl(
+                      method = "repeatedcv", 
+                      number = 10,
+                      repeats = 2))
+
+#caret train models by tag (google)
+plot(bike.model)
+
+preds <- predict(bike.model, newdata = bike %>% filter(id == "test"))
+submission <- data.frame(datetime = bike %>% filter(id == "test") %>% pull(datetime),
+                         count = preds)
+
+write.csv(x = submission, file = "./MyFirstSubmission.csv", row.names = FALSE)
+
+
+
+
+
+
+
+
+
+
+
